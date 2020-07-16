a3173e3dda71384de959efdef99ca2e8ce17def9
Maribelle Leong <maribelle.leong@gmail.com>
2020-07-16 14:38:57 -0300

    removed propTypes check

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,11 +1,6 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
 
 export default class Loading extends Component {
-  static propTypes = {
-    prop: PropTypes,
-  };
-
   render() {
     return <section className='loading'>Loading</section>;
   }
