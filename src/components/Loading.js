bc49517e19fa3b5843f28153b61f2d288b7efac6
Maribelle Leong <maribelle.leong@gmail.com>
2020-07-16 14:19:28 -0300

    added Loading component

---
+++ src/components/Loading.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+export default class Loading extends Component {
+  static propTypes = {
+    prop: PropTypes,
+  };
+
+  render() {
+    return <section className='loading'>Loading</section>;
+  }
+}
